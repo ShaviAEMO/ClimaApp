31ba27747924a1e68bdcba23aa509c9193987530
¨ShaviAEMO¨ <robotemma88@gmail.com>
2022-08-08 21:40:42 -0500

    primer push

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const {  inquirerMenu, pausa } = require("./helpers/inquirer");
+require('dotenv').config()
+const {  inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 
 const main = async() => {
+
+    const busquedas = new Busquedas();
     let opt
 
     do{
          opt = await  inquirerMenu();
-        console.log({opt});
+
+        switch(opt){
+            case 1:
+                //mostrar mensaje
+            const lugar = await leerInput('Ciudad: ')
+            await busquedas.ciudad(lugar);
+            //buscar los lugares
+
+            //seleccionar el lugar
+
+            //clima
+
+            //mostrar resultados
+               console.log('\n Infomacion de la ciudad \n'.green);
+               console.log('Ciudad: ',);
+               console.log('Lat: ',);
+               console.log('Lng: ',);
+               console.log('Temperatura: ',);
+               console.log('Min: ',);
+               console.log('Max: ',);
+
+
+            break;
+        }
 
         if(opt !== 0) await pausa();
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,13 +1,50 @@
+const axios = require('axios');
 
 
 
 
-
-class busqueda{
+class Busquedas{
 
     historial = ['Madrid', 'londres','Berlin'];
+
+    constructor (){
+/*TODO:
+leer base de datos
+*/
+ 
+}
+
+get paramsMapbox(){
+    return {
+        'limit':6,
+        'language': 'es',
+        'access_token': process.env.MAPBOX_KEY
+    
+    }
+}
+
+async ciudad(lugar=''){
+ //console.log('Ciudad',lugar); 
+ try{
+
+    const instance = axios.create({
+        baseURL:`https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapbox
+ })
+    const resp= await instance.get();
+    console.log(resp.data);
+    
+    return[];
+ }catch(error){
+console.log('El servicio no esta disponiblea')
+return[];
+
+ }  
+
+}
+
 }
 
 
 
-module.exports = busqueda;
+module.exports = Busquedas;
