3e5f5c92e573c42dd3cb95a961e323b2d13e7f28
¨ShaviAEMO¨ <robotemma88@gmail.com>
2022-08-10 12:12:00 -0500

    2 push

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const {  inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const {  inquirerMenu, pausa, leerInput, listarLugares } = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
 
 
@@ -14,19 +14,22 @@ const main = async() => {
         switch(opt){
             case 1:
                 //mostrar mensaje
-            const lugar = await leerInput('Ciudad: ')
-            await busquedas.ciudad(lugar);
+            const termino = await leerInput('Ciudad: ')
+            
             //buscar los lugares
-
+            const lugares = await busquedas.ciudad(termino);
+            
             //seleccionar el lugar
-
+            const id = await listarLugares(lugares);
+            const lugarSel = lugares.find(l => l.id === id);
+            console.log({lugarSel});
             //clima
 
             //mostrar resultados
                console.log('\n Infomacion de la ciudad \n'.green);
-               console.log('Ciudad: ',);
-               console.log('Lat: ',);
-               console.log('Lng: ',);
+               console.log('Ciudad: ',lugarSel.nombre);
+               console.log('Lat: ',lugarSel.lat);
+               console.log('Lng: ',lugarSel.lng);
                console.log('Temperatura: ',);
                console.log('Min: ',);
                console.log('Max: ',);
